3adffe0d615d75b905dbe8d70cdba800478d2b2d
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-10-23 19:51:11 +0300

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""returns information about his/her TODO list progress.
+"""
+
+
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv
+    res = get("https://jsonplaceholder.typicode.com/users")
+    data = res.json()
+    all_tasks = 0
+    completed = 0
+    tasks = []
+
+    for n in data:
+        if n.get("id") == int(argv[1]):
+            employee = n.get("name")
+
+    res = get("https://jsonplaceholder.typicode.com/todos")
+    data1 = res.json()
+
+    for i in data1:
+        if i.get("userId") == int(argv[1]):
+            all_tasks += 1
+            if i.get("completed") is True:
+                completed += 1
+                tasks.append(i.get("title"))
+
+    print("Employee {} is done with tasks({}/{}):".format(employee, completed,
+                                                          all_tasks))
+    for i in tasks:
+        print("\t {}".format(i))
