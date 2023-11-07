6f2fda49ddee976d2699b3325e413e09f85480e5
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-11-07 13:29:46 +0200

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,17 +10,14 @@ def top_ten(subreddit):
     Function that queries the Reddit API
     - If not a valid subreddit, print None.
     """
-    try:
-        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-        headers = {"User-Agent": "My-User-Agent"}
-        res = requests.get(url, headers=headers, params={"limit": 10})
-        data = res.json()
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
+    req = requests.get(url, headers=headers, params={"limit": 10})
+    if req.status_code == 200:
+        data = req.json()
         titles = data["data"]["children"]
         for title in titles:
             print(title["data"]["title"])
-        if data["error"] == 404:
-            print(None)
-            return None
-        return titles
-    except Exception as e:
-        return None
+    else:
+        print("None")
