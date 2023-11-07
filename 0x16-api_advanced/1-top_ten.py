ec904a5f2318c9f79d9e6a01253417e762a29091
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-11-07 12:48:29 +0200

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+1-top_ten
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, print None.
+    """
+    try:
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+        headers = {"User-Agent": "My-User-Agent"}
+        res = requests.get(url, headers=headers, params={"limit": 10})
+        data = res.json()
+        titles = data["data"]["children"]
+        for title in titles:
+            print(title["data"]["title"])
+        return titles
+    except Exception as e:
+        return None
