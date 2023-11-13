898593d0dc1314a57df2295865c60f549d2e67a4
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-11-13 22:55:51 +0200

    0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Increases the amount of traffic an Nginx server can handle.
+
+exec { 'fix-ulimit':
+    command => 'sed -i "s/15/4096/" /etc/default/nginx',
+    path    => '/usr/local/bin/:/bin/',
+}
+service { 'nginx':
+    ensure => 'running',
+    enable => true,
+    require => Exec['fix-ulimit'],
+}
